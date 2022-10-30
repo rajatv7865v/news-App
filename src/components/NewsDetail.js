f191a99260d417a06e76a081b112719c4e4275d2
Rajat Verma <rajatv785v@gmail.com>
2022-10-30 21:22:57 +0530

    code

--- src/components/NewsDetail.js
+++ src/components/NewsDetail.js
@@ -15,7 +15,8 @@ const NewsDetail = () => {
     useEffect(() => {
      const getapi=async()=>{
       let getResponse=await axios.get(
-        "https://newsapi.org/v2/everything?q=tesla&from=2022-09-29&sortBy=publishedAt&apiKey=ee1b0385760345cfb7c60c76b19e826d"
+        " https://newsapi.org/v2/everything?q=tesla&from=2022-09-30&sortBy=publishedAt&apiKey=4be6485d8680485ab576eb5ea214679c"
+        // "https://newsapi.org/v2/everything?q=tesla&from=2022-09-29&sortBy=publishedAt&apiKey=ee1b0385760345cfb7c60c76b19e826d"
       )
       getResponse=await getResponse.data
       // console.log("data ",getResponse)
